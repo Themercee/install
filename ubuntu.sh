2a6cc0ac0b3f6dad5c88432352d553fd50655cd9
Themercee <jimmymd92@gmail.com>
2020-08-31 09:25:36 -0400

    Update ubuntu.sh
    Add diskonaut

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,13 @@
 
 apt-get update -y && apt-get upgrade -y
 
-apt-get install tmux git build-essential python zsh ruby 
+apt-get install tmux git build-essential python zsh ruby cargo
 
 # install zsh
 curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+
+##
+# System tools
+##
+# install diskonaut
+cargo install diskonaut
